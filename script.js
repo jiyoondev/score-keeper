e0a578f8bf3248c92937bde5fa85c623712617b7
jiyoondev <jiyoon.jang.dev@gmail.com>
2021-04-09 23:41:28 +0900

    Create function reset & winner loser classlist

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const p2Button = document.querySelector("#p2Button");
 const resetButton = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
-const playToSelect = document.querySelector("#playto");
+const winningScoreSelect = document.querySelector("#playto");
 
 let p1Score = 0;
 let p2Score = 0;
@@ -15,6 +15,8 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -25,15 +27,31 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
 });
 
-resetButton.addEventListener("click", function () {
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+//this is the callback
+//passing this anonymous function as a callback and excute reset inside
+
+resetButton.addEventListener("click", reset);
+//not executing, passing a reference
+//it will execute reset when time is come
+//passing as callback
+
+function reset() {
   isGameOver = false;
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-});
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+}
